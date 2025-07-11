a7a7861c95e370ccbe6075db2be849412e5bdab1
LayGord <TWISTBOY08@gmail.com>
2025-07-11 16:11:09 +0300

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,106 @@
+"use strict";
+// 1.
+function hasUniqueChars(str) {
+    return new Set(str).size == str.length;
+}
+// тестируем
+console.log(`hasUniqueChars("abcde"): ${hasUniqueChars("abcde")}`); // True
+console.log(`hasUniqueChars("hello"): ${hasUniqueChars("hello")}`); // False
+// 2.
+function areAnagrams(a, b) {
+    return a.length === b.length &&
+        [...a].sort().join('') === [...b].sort().join('');
+}
+// тестируем
+console.log(`areAnagrams("listen", "silent"): ${areAnagrams("listen", "silent")}`); // True
+console.log(`areAnagrams("hello", 'hella'): ${areAnagrams("hello", 'hella')}`); // False
+// 3.
+// эмуляция ошибок
+async function doError(errorMessage) {
+    return Promise.reject(new Error(`${errorMessage}`));
+}
+async function getUserInfo(userId) {
+    console.log(`Получение информации о пользователе ${userId}...`);
+    //await doError('Ошибка при получении информации о пользователе!');
+    return new Promise(resolve => setTimeout(() => resolve({ id: userId, name: 'Alice', email: 'alice@example.com' }), 100));
+}
+async function getUserSettings(userId) {
+    console.log(`Получение настроек пользователя ${userId}...`);
+    //await doError('Ошибка при получении настроек пользователя!')
+    return new Promise(resolve => setTimeout(() => resolve({ theme: 'dark', language: 'en' }), 150));
+}
+async function getUserPermissions(userId) {
+    console.log(`Получение прав пользователя ${userId}...`);
+    //await doError('Ошибка при получении прав пользователя!')
+    return new Promise(resolve => setTimeout(() => resolve(["read", "write"]), 200));
+}
+async function buildUserSnapshot(userId) {
+    try {
+        const [userInfo, userSettings, userPermissions] = await Promise.all([
+            getUserInfo(userId),
+            getUserSettings(userId),
+            getUserPermissions(userId)
+        ]);
+        return {
+            id: userId,
+            name: userInfo.name,
+            email: userInfo.email,
+            settings: userSettings,
+            permissions: userPermissions,
+        };
+    }
+    catch (error) {
+        throw new Error(`Не удалось получить слепок пользователя ${userId}: ${error.message || 'неизвестная ошибка'}`);
+    }
+}
+// тестируем
+(async () => {
+    try {
+        const snapshot = await buildUserSnapshot("u-123");
+        console.log(snapshot);
+    }
+    catch (error) {
+        console.error("Произошла ошибка:", error.message);
+    }
+})();
+// вспомогательный метод проверки на валидную дату
+function isValidISODate(dateString) {
+    try {
+        if (typeof dateString === 'string') {
+            const date = new Date(dateString);
+            if (isNaN(date.getTime()) && isNaN(date.getDate())) {
+                return false; // Date() не смог создать валидную дату (например, "2023-02-30")
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+function isValidTaskMessage(obj) {
+    const options = ["low", "medium", "high"];
+    return (typeof obj === 'object' && obj != null &&
+        'id' in obj && typeof obj.id === 'string' &&
+        'title' in obj && typeof obj.title === 'string' &&
+        'createdAt' in obj && isValidISODate(obj.createdAt) &&
+        'priority' in obj && typeof obj.priority === 'string'
+        && options.includes(obj.priority));
+}
+// тестируем
+console.log(isValidTaskMessage({
+    id: "123",
+    title: "Do something",
+    createdAt: "2025-07-10T12:00:00Z",
+    priority: "high"
+})); // true
+console.log(isValidTaskMessage({
+    id: 123,
+    title: "Missing createdAt",
+    priority: "low"
+})); // false (id не строка, нет createdAt)
+console.log(isValidTaskMessage(null)); // false
